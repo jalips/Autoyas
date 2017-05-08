ff5f3d9c17b0eeba22c16a344332461c62e0815c
jalips <link_521@hotmail.fr>
2017-05-08 23:12:04 +0200

    start rebuild

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 /**
  * Main program with setup and loop
  */
-#include "conf/libManager.cpp"
-#include "conf/coreManager.cpp"
+
+#include "Arduino.h"
+
+#include "banner/trollFace.h"
+//#include "conf/libManager.h"
+//#include "conf/coreManager.cpp"
 
 void setup()
 {
   Serial.begin(115200);
-
+  trollFace();
 }
 
 void loop()

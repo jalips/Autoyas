8f22dd573e961a586f68665abdc832ec31ec0fff
jalips <link_521@hotmail.fr>
2017-05-09 11:34:15 +0200

    Add comms

--- src/conf/libManager.h
+++ src/conf/libManager.h
@@ -1,5 +1,7 @@
-/**
- * Lib manager to load all files & function
- */
+/************************************************************************
+* Lib manager to load all files & function
+***********************************************************************/
 
 #include "../banner/bannerManager.h"
+
+#include "../sensor/tempHydro.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,25 @@
-/**
- * Main program with setup and loop
- */
+ /************************************************************************
+  * Main program with setup and loop
+  ***********************************************************************/
 
 #include "Arduino.h"
 
+#include "conf/confManager.h"
 #include "conf/libManager.h"
-//#include "conf/coreManager.cpp"
 
 void setup()
 {
   Serial.begin(115200);
   displayBanner();
+
+  initSetupTemp();
 }
 
 void loop()
 {
   Serial.println("Hello world");
   delay(2000);
+
+  getTemp();
+
 }

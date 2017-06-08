a81045709074342c5a1bf7e4d96b5dead8bc9fb9
jalips <link_521@hotmail.fr>
2017-06-08 20:00:28 +0200

    Add water level info

--- src/core/sensor/water.h
+++ src/core/sensor/water.h
@@ -8,6 +8,7 @@ const int capteur = 0;
 int loopWater(){
   // Lecture de la valeur du capteur
   int val = analogRead (capteur);
+  Serial.println ("Water level : ");
   Serial.println (val);
   // Attendre 5s avant la prochaine lecture
   delay (1000);
